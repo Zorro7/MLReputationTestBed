d78ee02954c4a3d42ed4fce6e28b89fc9cc2c2de
Phil Taylor <ptay89@gmail.com>
2016-03-15 11:01:08 +0000

    Added some utility classes

---
+++ src/jaspr/utilities/NamedEntity.scala
@@ -0,0 +1,29 @@
+package jaspr.utilities
+
+import scala.collection.mutable
+
+/**
+ * Created by phil on 26/01/16.
+ */
+
+object NamedEntity {
+
+  val indexes = new mutable.HashMap[Class[_ <: NamedEntity], Int]
+
+  def nextIndex(ne: NamedEntity): Int = {
+    val x = indexes.getOrElse(ne.getClass, 0) + 1
+    indexes.put(ne.getClass, x)
+    x
+  }
+}
+
+
+trait NamedEntity {
+
+  val id: Int = NamedEntity.nextIndex(this)
+  val name: String = id.toString
+
+  override def toString(): String = {
+    this.getClass.getSimpleName + name
+  }
+}
