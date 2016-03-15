d24c5d60d972fc5be3ee5b2f041d449b64b08943
Phil Taylor <ptay89@gmail.com>
2016-03-15 10:47:58 +0000

    Some cleaning in package object

--- src/jaspr/package.scala
+++ src/jaspr/package.scala
@@ -1,20 +1,16 @@
-import jaspr.core.Configuration
 
 /**
  * Created by phil on 26/01/16.
  */
 package object jaspr {
 
-  val debugLevel = Configuration.debug
+  val debugLevel = 0
 
   def debug(str: String, objs: Any*): Unit = {
     debug(5, str, objs:_*)
   }
 
-  // Coded like this to allow a message followed by other strings/objects.
-  // This avoids calling toString on everything when debug is disabled.
   def debug(level: Int, str: String, objs: Any*): Unit = {
     if (level > debugLevel) println(objs.mkString(str, " :: ", ""))
   }
-
 }
