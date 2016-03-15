ff37a752acd59e4a0f59713e321d1df2cbb2a9f1
Phil Taylor <ptay89@gmail.com>
2016-03-15 15:59:51 +0000

    removed simulation pointed in properties

--- src/jaspr/core/agent/Properties.scala
+++ src/jaspr/core/agent/Properties.scala
@@ -1,14 +1,9 @@
 package jaspr.core.agent
 
-import jaspr.core.Simulation
-import jaspr.utilities.NamedEntity
-
 /**
  * Created by phil on 15/03/16.
  */
 trait Properties {
-
-  val simulation: Simulation
 
   def properties: Map[Property, Double]
   def advertProperties: Map[Property, AnyVal]
