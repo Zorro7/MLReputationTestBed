15b095f52626eefdaa56446d5d1ced04503900e9
Phil Taylor <ptay89@gmail.com>
2016-03-16 14:25:02 +0000

    git q!

--- src/jaspr/simplesim/SimpleConfiguration.scala
+++ src/jaspr/simplesim/SimpleConfiguration.scala
@@ -2,21 +2,27 @@ package jaspr.simplesim
 
 import jaspr.core.strategy.Strategy
 import jaspr.core.{MultiConfiguration, Simulation, Configuration}
-import jaspr.simplesim.strategy.NoStrategy
+import jaspr.simplesim.strategy.{Fire, NoStrategy}
 
 /**
  * Created by phil on 15/03/16.
  */
-class SimpleConfiguration extends MultiConfiguration with Configuration {
+class SimpleConfiguration(val strategy: Strategy) extends Configuration {
   override def newSimulation(): Simulation = {
     new SimpleSimulation(this)
   }
 
   override val numSimulations: Int = 1
-  override val numRounds: Int = 10
-  override val strategy: Strategy = new NoStrategy()
+  override val numRounds: Int = 250
+
+}
+
+
+class SimpleMultiConfiguration extends MultiConfiguration {
+  override val directComparison = true
 
   override lazy val configs: Seq[Configuration] =
-    new SimpleConfiguration() ::
+    new SimpleConfiguration(new Fire) ::
+    new SimpleConfiguration(new NoStrategy) ::
       Nil
 }
