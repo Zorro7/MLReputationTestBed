b07a664ff4b75ce76088a3349eb82e319d80daa3
Phil Taylor <ptay89@gmail.com>
2016-03-16 14:24:26 +0000

    Added exploration traits

--- src/jaspr/simplesim/strategy/NoStrategy.scala
+++ src/jaspr/simplesim/strategy/NoStrategy.scala
@@ -2,13 +2,13 @@ package jaspr.simplesim.strategy
 
 import jaspr.core.Network
 import jaspr.core.service.{TrustAssessment, ServiceRequest, ClientContext}
-import jaspr.core.strategy.{StrategyInit, Strategy}
+import jaspr.core.strategy.{NoExploration, StrategyInit, Strategy}
 import jaspr.utilities.Chooser
 
 /**
  * Created by phil on 16/03/16.
  */
-class NoStrategy extends Strategy {
+class NoStrategy extends Strategy with NoExploration {
 
   override def initStrategy(network: Network, context: ClientContext): StrategyInit = {
     new StrategyInit
@@ -18,13 +18,9 @@ class NoStrategy extends Strategy {
     new TrustAssessment(request, Chooser.randomDouble(0,1))
   }
 
-  override def select(orderedAssessments: Seq[TrustAssessment]): TrustAssessment = {
-    orderedAssessments.head
-  }
-
   override def possibleRequests(network: Network, context: ClientContext): Seq[ServiceRequest] = {
-    network.providers.map(new ServiceRequest(context.client, _, context.round, 1,
-      context.properties
-    ))
+    network.providers.map(
+      new ServiceRequest(context.client, _, context.round, 1, context.properties)
+    )
   }
 }
