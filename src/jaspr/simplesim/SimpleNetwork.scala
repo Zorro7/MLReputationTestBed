aa89636f27e416938be2f8de8b315965418f8b80
Phil Taylor <ptay89@gmail.com>
2016-03-16 14:27:18 +0000

    Minor change to utility computation

--- src/jaspr/simplesim/SimpleNetwork.scala
+++ src/jaspr/simplesim/SimpleNetwork.scala
@@ -11,7 +11,7 @@ class SimpleNetwork(val simulation: Simulation) extends Network {
 
   override def utility(): Double = agents.map(_.utility).sum
 
-  override val agents: Seq[Agent] = List.fill(1)(new SimpleAgent(simulation))
+  override val agents: Seq[Agent] = List.fill(10)(new SimpleAgent(simulation))
 
   override val clients: Seq[Client] = agents.map(_.asInstanceOf[SimpleAgent])
   override val providers: Seq[Provider] = agents.map(_.asInstanceOf[SimpleAgent])
