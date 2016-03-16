641cbccb3ac9ec5227fc371dfdcde0285e5cf203
Phil Taylor <ptay89@gmail.com>
2016-03-16 13:33:55 +0000

    Added core interfaces for strategy

---
+++ src/jaspr/core/strategy/Strategy.scala
@@ -0,0 +1,35 @@
+package jaspr.core.strategy
+
+import jaspr.core.Network
+import jaspr.core.service.{ServiceRequest, TrustAssessment, ClientContext}
+import jaspr.utilities.Chooser
+
+/**
+ * Created by phil on 16/03/16.
+ */
+abstract class Strategy {
+
+  val name: String = this.getClass.getSimpleName
+  override def toString = name
+
+  def initStrategy(network: Network, context: ClientContext): StrategyInit
+  def computeAssessment(init: StrategyInit, request: ServiceRequest): TrustAssessment
+
+  def select(orderedAssessments: Seq[TrustAssessment]): TrustAssessment
+  def possibleRequests(network: Network, context: ClientContext): Seq[ServiceRequest]
+
+  def assessReputation(network: Network, context: ClientContext): TrustAssessment = {
+    val requests = possibleRequests(network, context)
+    val init = initStrategy(network, context)
+    val orderedProviders = rank(init, requests)
+    select(orderedProviders)
+  }
+
+  def rank(init: StrategyInit, requests: Seq[ServiceRequest]): Seq[TrustAssessment] = {
+    val assessments = requests.map(computeAssessment(init, _))
+    Chooser.shuffle(assessments).sortBy(x =>
+      if (x.trustValue.isNaN) Double.MinValue else x.trustValue
+    ).reverse
+  }
+
+}
---
+++ src/jaspr/core/strategy/StrategyInit.scala
@@ -0,0 +1,6 @@
+package jaspr.core.strategy
+
+/**
+ * Created by phil on 16/03/16.
+ */
+class StrategyInit
