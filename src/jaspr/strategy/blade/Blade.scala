5a493e3d8315b7349ab5b75a6c78c763f53b325a
Phil Taylor <ptay89@gmail.com>
2016-03-25 18:10:18 +0100

    Actually added implementation of BLADE

---
+++ src/jaspr/strategy/blade/Blade.scala
@@ -0,0 +1,84 @@
+package jaspr.strategy.blade
+
+import jaspr.core.Network
+import jaspr.core.agent.{Client, Provider}
+import jaspr.core.service.{ClientContext, TrustAssessment, ServiceRequest}
+import jaspr.core.strategy.{StrategyInit, Exploration}
+import jaspr.strategy.{RatingStrategy, CompositionStrategy}
+import jaspr.utilities.Dirichlet
+import jaspr.utilities.matrix.{RowVector, Matrix}
+
+/**
+ * Created by phil on 25/03/16.
+ */
+class Blade extends CompositionStrategy with RatingStrategy with Exploration with BladeCore {
+
+  override val explorationProbability: Double = 0.1
+
+  override val numBins: Int = 2
+  override val lower: Double = -1d
+  override val upper: Double = 1d
+
+  override def initStrategy(network: Network, context: ClientContext): StrategyInit = {
+    val direct = toRatings(context.client.getProvenance(context.client))
+    val witness = toRatings(network.gatherProvenance(context.client))
+
+    val trustees: Seq[Provider] = direct.map(_.provider).distinct ++ witness.map(_.provider).distinct
+    val witnesses: Seq[Client] = witness.map(_.client).distinct
+
+    val dirPrior = this.getDirectPrior(context)
+    val repPrior = this.getRepPrior(context.client)
+    val dirModels = this.getDirectModels(direct, dirPrior)
+
+    val repModels = this.getRepModels(witness, trustees, witnesses)
+
+    val repMatrix: Map[(Client,Provider),Matrix] =
+      (for (tr <- witnesses; te <- trustees) yield {
+        val model = repModels.getOrElse((tr,te), repPrior)
+        val opinionObs: RowVector = model.alpha @- repPrior.alpha
+        (tr,te) -> dirModels.getOrElse(te, dirPrior).mean().transpose() @* opinionObs
+      }).toMap
+
+    new BladeInit(
+      context,
+      trustees, witnesses,
+      dirPrior, repPrior,
+      dirModels, repModels, repMatrix
+    )
+  }
+
+
+  override def compute(init: StrategyInit, request: ServiceRequest): TrustAssessment = {
+    val directModels = init.asInstanceOf[BladeInit].directModels
+    val repModels = init.asInstanceOf[BladeInit].repModels
+    val trustees = init.asInstanceOf[BladeInit].trustees
+    val witnesses = init.asInstanceOf[BladeInit].witnesses
+    val dirModelPrior = init.asInstanceOf[BladeInit].dirModelPrior
+    val repModelPrior = init.asInstanceOf[BladeInit].repModelPrior
+
+    val repMatrix = init.asInstanceOf[BladeInit].repMatrix
+    val trustee = request.provider
+
+    val priorModel = directModels.getOrElse(trustee, dirModelPrior)
+
+    val likelihoods = for (tr <- witnesses) yield {
+      val cpt: Matrix = repMatrix
+        .filter(x => x._1._1 == tr && x._1._2 != trustee).values
+        .foldLeft(new Matrix(dirModelPrior.size,repModelPrior.size,1d))(_ @+ _)
+
+      val cptnorm = cpt @/ cpt.colsum().sum
+
+      val rcond: Matrix = divRcondRows(divMeanPrior(priorModel, cptDivRowSum(cptnorm)))
+
+      val sourceAlpha: RowVector = repModels.getOrElse((tr,trustee), repModelPrior).alpha @- 1d
+
+      (rcond @* sourceAlpha).rowsum().transpose() // total likelihood
+    }
+
+    val postModel = new Dirichlet(likelihoods.foldLeft(priorModel.alpha)(_ @+ _), priorModel.domain)
+
+    new TrustAssessment(request, postModel.expval())
+  }
+
+
+}
---
+++ src/jaspr/strategy/blade/BladeCore.scala
@@ -0,0 +1,59 @@
+package jaspr.strategy.blade
+
+import jaspr.core.agent.{Provider, Client}
+import jaspr.core.service.ClientContext
+import jaspr.core.strategy.StrategyInit
+import jaspr.strategy.Rating
+import jaspr.utilities.{Dirichlet, Discretization}
+import jaspr.utilities.matrix.Matrix
+
+/**
+ * Created by phil on 01/11/15.
+ */
+trait BladeCore extends Discretization {
+
+
+  class BladeInit(context: ClientContext,
+                  val trustees: Seq[Provider],
+                  val witnesses: Seq[Client],
+                  val dirModelPrior: Dirichlet,
+                  val repModelPrior: Dirichlet,
+                  val directModels: Map[Provider, Dirichlet],
+                  val repModels: Map[(Client,Provider), Dirichlet],
+                  val repMatrix: Map[(Client,Provider),Matrix]
+                   ) extends StrategyInit(context)
+
+
+
+  def getRepPrior(client: Client): Dirichlet = {
+    new Dirichlet(numBins)
+  }
+  def getDirectPrior(context: ClientContext): Dirichlet = {
+    new Dirichlet(numBins)
+  }
+
+  def getDirectModels(directRatings: Seq[Rating], prior: Dirichlet): Map[Provider,Dirichlet] = {
+//    val provratings: Map[AbstractAgent,List[Rating]] = directRatings.groupBy(_.provider)
+//    val asdf: Map[AbstractAgent, Map[Double, Int]] = provratings.mapValues(x => x.groupBy(y => discretize(y.rating)).mapValues(_.size+1))
+//    val out = asdf.mapValues(x => new Dirichlet((0 until numbins).map(y => x.getOrElse(y, 1).toDouble)))
+    directRatings.groupBy(_.provider).mapValues(x => prior.observe(x.map(y => discretizeDouble(y.rating))))
+  }
+
+  def getRepModels(witnessReports: Seq[Rating],
+                   trustees: Seq[Provider],
+                   witnesses: Seq[Client]): Map[(Client,Provider),Dirichlet] = {
+    (for (w <- witnesses; t <- trustees) yield {
+      val r =  witnessReports.filter(x => x.client == w && x.provider == t)
+      (w,t) -> getRepPrior(w).observe(
+        witnessReports.filter(x => x.client == w && x.provider == t).map(x => discretizeDouble(x.rating))
+      )
+    }).toMap
+  }
+
+  def cptDivRowSum(cpt: Matrix) = cpt @/ cpt.rowsum()
+  def divMeanPrior(priorModel: Dirichlet, rcond: Matrix) = rcond @* priorModel.mean().transpose()
+  def divRcondRows(rcond: Matrix) = rcond @/ rcond.colsum()
+
+
+
+}
