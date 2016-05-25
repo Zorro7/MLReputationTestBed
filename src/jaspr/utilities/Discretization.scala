95b132c56a1c1526b300f9812792e7f880a9ef69
Phil Taylor <ptay89@gmail.com>
2016-05-25 16:57:44 +0100

    Bugfix to discretation for (I think) Scala 2.11.5, where last element in discVals was missing

--- src/jaspr/utilities/Discretization.scala
+++ src/jaspr/utilities/Discretization.scala
@@ -9,8 +9,10 @@ trait Discretization {
   val upper: Double
   val lower: Double
   val numBins: Int
+  lazy val step: Double = (upper-lower) / numBins
 
-  lazy val discVals = Range.Double(lower, upper, (upper-lower)/numBins).map(_.toString)
+//  lazy val discVals: Seq[String] = Range.Double(lower, upper, step).map(_.toString)
+  lazy val discVals: Seq[String] = for (bin <- 0 until numBins) yield (lower + bin * step).toString
 
   def discretizeInt(x: Double): Int = {
     discretizeDouble(x).toInt
