850b37457cec4bd8ee0e89875a698c754fb7c7e8
Phil Taylor <ptay89@gmail.com>
2016-06-09 16:20:31 +0100

    Added capability to take zipped args with dicts

--- src/python/makepbs.py
+++ src/python/makepbs.py
@@ -10,11 +10,16 @@ def makecommands(base, args):
 		ns,vs = args.iteritems().next()
 		if not isinstance(vs, list):
 			vs = [vs]
-		del args[a]
+		del args[ns]
 		return [cmd+" "+makearg(ns,v) for cmd in makecommands(base, args) for v in vs]
 
 def makearg(ns, v):
-	return ' '.join(["--"+str(n)+" '"+str(v)+"'" for n in ns.split(",")])
+	if isinstance(v, dict):
+		return ''.join(makecommands("", v))[1:]
+	elif isinstance(v, list):
+		return ''.join(["--"+str(n)+" '"+str(vn)+"'" for n,vn in zip(ns.split(","),v)])
+	else:
+		return ' '.join(["--"+str(n)+" '"+str(v)+"'" for n in ns.split(",")])
 
 def makeoutput(cmd):
 	return '_'.join([x for x,i in zip(cmd.split(" ")[5:],xrange(0,len(cmd.split(" ")))) if i % 2 == 0]).replace("weka.classifiers.", "")
