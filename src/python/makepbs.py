85deb726008798918a06423a108092e45c2838f8
Phil Taylor <ptay89@gmail.com>
2016-06-09 12:26:05 +0100

    Added capability for zipped arg values

--- src/python/makepbs.py
+++ src/python/makepbs.py
@@ -1,18 +1,20 @@
 import sys
 
 def makecommands(base, args):
-    if len(args) == 1:
-        a,vs = args.iteritems().next()
-        if not isinstance(vs, list):
-            vs = [vs]
-        return [base+" --"+a+" '"+str(v)+"'" for v in vs]
-    else:
-        a,vs = args.iteritems().next()
-        if not isinstance(vs, list):
-            vs = [vs]
-        del args[a]
-        return [cmd+" --"+a+" '"+str(v)+"'" for cmd in makecommands(base, args) for v in vs]
-        # return [makecommands(base+" "+a+" "+str(v), args) for v in vs]
+	if len(args) == 1:
+		ns,vs = args.iteritems().next()
+		if not isinstance(vs, list):
+			vs = [vs]
+		return [base+" "+makearg(ns,v) for v in vs]
+	else:
+		ns,vs = args.iteritems().next()
+		if not isinstance(vs, list):
+			vs = [vs]
+		del args[a]
+		return [cmd+" "+makearg(ns,v) for cmd in makecommands(base, args) for v in vs]
+
+def makearg(ns, v):
+	return ' '.join(["--"+str(n)+" '"+str(v)+"'" for n in ns.split(",")])
 
 def makeoutput(cmd):
-    return '_'.join([x for x,i in zip(cmd.split(" ")[5:],xrange(0,len(cmd.split(" ")))) if i % 2 == 0]).replace("weka.classifiers.", "")
+	return '_'.join([x for x,i in zip(cmd.split(" ")[5:],xrange(0,len(cmd.split(" ")))) if i % 2 == 0]).replace("weka.classifiers.", "")
