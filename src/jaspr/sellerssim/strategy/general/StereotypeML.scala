25ad5b85e15e130b2a360602eab79d3b61e5a9b1
Phil Taylor <ptay89@gmail.com>
2016-06-30 01:02:51 +0100

    Added stereotype ML

---
+++ src/jaspr/sellerssim/strategy/general/StereotypeML.scala
@@ -0,0 +1,46 @@
+package jaspr.sellerssim.strategy.general
+
+import jaspr.core.Network
+import jaspr.core.agent.Provider
+import jaspr.core.provenance.{RatingRecord, ServiceRecord, Record}
+import jaspr.core.service.{ServiceRequest, ClientContext}
+import jaspr.core.strategy.StrategyInit
+
+/**
+ * Created by phil on 30/06/16.
+ */
+trait StereotypeML extends SingleModelStrategy {
+
+  val baseStrategy: SingleModelStrategy
+
+  override def getRecords(network: Network, context: ClientContext): Seq[Record] = {
+    baseStrategy.getRecords(network, context)
+  }
+
+  override def makeTrainRow(baseRecord: Record): Seq[Any] = {
+    val record = baseRecord.asInstanceOf[ServiceRecord with RatingRecord]
+    baseStrategy.makeTrainRow(baseRecord) ++
+      adverts(record.service.request.provider)
+  }
+
+  override def makeTestRow(init: StrategyInit, request: ServiceRequest): Seq[Any] = {
+    baseStrategy.makeTestRow(init, request) ++
+      adverts(request.provider)
+  }
+
+  def adverts(provider: Provider): List[Any] = {
+    provider.advertProperties.values.map(_.value).toList
+  }
+}
+
+class BasicStereotype extends StereotypeML {
+  override val baseStrategy = new BasicML
+}
+
+class FireLikeStereotype extends StereotypeML {
+  override val baseStrategy = new FireLike
+}
+
+class TravosLikeStereotype extends StereotypeML {
+  override val baseStrategy = new TravosLike
+}
