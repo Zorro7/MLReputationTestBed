2aeaf3393a1d8d165a9565328c2c5d60c4b08764
Phil Taylor <ptay89@gmail.com>
2016-11-23 01:18:10 +0000

    Other tinkering

--- src/matlab/DistributionTesting.m
+++ src/matlab/DistributionTesting.m
@@ -1,6 +1,6 @@
 clear all
 
-n = 100;
+n = 50;
 rounds = 5000;
 
 propMin = 0;
@@ -9,10 +9,10 @@ capMin = 0;
 capMax = 1;
 prefMin = 0;
 prefMax = 1;
-sigma = 0.1;
+sigma = 0.0;
 
-fixedPreference = 1;
-baseUtility = 1;
+fixedPreference = 0.5;
+baseUtility = 0.5;
 
 results = zeros(1,rounds);
 for round = 1:rounds
@@ -26,18 +26,21 @@ for round = 1:rounds
     
     capabilities = (properties+simCapabilities)/2;
 
-    utilities = baseUtility - abs(capabilities-preferences);
+    utilities = baseUtility - (preferences-capabilities);
+%     utilities = (preferences < capabilities);
 %     utilities = abs(capabilities-preferences) < 0.5;
     
     results(round) = mean(utilities);
 end
 meanFixed = mean(results);
+subplot(2,1,1),histogram(results)
 
 results = zeros(1,rounds);
 for round = 1:rounds
     capMean = capMin+(capMax-capMin)*rand(1,1);
     propMean = propMin+(propMax-propMin)*rand(1,1);
     prefMean = prefMin+(prefMax-prefMin)*rand(1,1);
+%     prefMean = randn(1,1)*0.1;
 
     simCapabilities = sigma*randn(1,n)+capMean;
     properties = sigma*randn(1,n)+propMean;
@@ -46,14 +49,15 @@ for round = 1:rounds
         
     capabilities = (properties+simCapabilities)/2;
 
-    utilities = baseUtility - abs(capabilities-preferences);
+    utilities = baseUtility - (preferences-capabilities);
+%     utilities = (preferences < capabilities);
 %     utilities = abs(preferences-capabilities) < 0.25;
     
     results(round) = mean(utilities);
 end
 
 meanSubjective = mean(results);
-
+subplot(2,1,2),histogram(results)
 [meanFixed,meanSubjective]
 
 
