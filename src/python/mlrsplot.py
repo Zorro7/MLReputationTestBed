90c520b67b4a36160d1ac65490098b5ec24e225b
Phil Taylor <phil@carolus.dcs.warwick.ac.uk>
2016-11-25 16:08:14 +0000

    plot changes for new results format

--- src/python/mlrsplot.py
+++ src/python/mlrsplot.py
@@ -13,54 +13,25 @@ if __name__ == "__main__":
     for filename in files:
         results.extend(loadprocessed(filename))
 
-    df = "{0:.1f}"
+    df = "{0:.5f}"
     strategies = [
-        ('Random.res',),
-        ('DirectBRS.res',),
-        ('DirectFire.res',),
-        ('DirectML-RF-2.res',),
-        ('DirectMLContext-RF-2.res',),
-        ('DirectMLStereotype-RF-2.res',),
-        # ('DirectMLStereotypeContracts-RF-2.res',),
-        # ('MLRSDirect-2.res',),
-        # ('MLRSDirectAds-2.res',),
-        # ('MLRSDirectContracts-2.res',),
-        ('Fire.res',),
-        ('BRS.res',),
-        ('TRAVOS.res',),
-        ('BLADE-2.res',),
-        ('HABIT-2.res',),
-        # ('ML-RF-2.res',),
-        # ('MLContext-RF-2.res',),
-        # ('MLStereotype-RF-2.res',),
-        # ('MLStereotyeContract-RF-2.res',),
-        ('MLRS-2.res',),
-        ('MLRSAds-2.res',),
-        # ('MLRSContracts-2.res',),
-        # ('MLRS-10.res',),
-        # ('MLRS-3.res',),
-        # ('MLRS-5.res',),
-        # ('MLRSAds-10.res',),
-        # ('MLRSAds-3.res',),
-        # ('MLRSAds-5.res',),
-        # ('MLRSContracts-10.res',),
-        # ('MLRSContracts-3.res',),
-        # ('MLRSContracts-5.res',),
-        # ('MLRSDirect-10.res',),
-        # ('MLRSDirect-3.res',),
-        # ('MLRSDirect-5.res',),
-        # ('MLRSDirectAds-10.res',),
-        # ('MLRSDirectAds-3.res',),
-        # ('MLRSDirectAds-5.res',),
-        # ('MLRSDirectContracts-10.res',),
-        # ('MLRSDirectContracts-3.res',),
-        # ('MLRSDirectContracts-5.res',),
+    ('Random.res',),
+    ('BRSDirect.res',),
+    ('FireDirect.res',),
+    ('BRS.res',),
+    ('Fire.res',),
+    ('TRAVOS.res',),
+    ('BLADE-2.res',),
+    ('HABIT-2.res',),
+    ('MLRS-M5.res',),
+    ('MLRS-NB2.res',),
+    ('MLRS-RF2.res',),
     ]
 
     
 
-    splt = split(results, "memoryLimit", "numTerms", "witnessRequestLikelihood", "noiseRange",
-                 "numPreferences", "numSimCapabilities", "providerAvailabilityLikelihood", "providerAttrition")
+    splt = split(results, "memoryLimit", "numTerms", "witnessesAvailable", "sigma",
+                 "numPreferences", "numSimCapabilities", "providersAvailable", "providerAttrition")
 
     print "%", splt.keys()
     # index = (0.1, 100, 20, 100, 10, 3, 3, "true", 1)
@@ -71,6 +42,7 @@ if __name__ == "__main__":
     # print index, len(splt)
 
     expsplt = split(splt, "resname")
+    print "%", expsplt.keys()
     print "%", [len(expsplt[strategy]) for strategy in strategies if strategy in expsplt]
 
 
@@ -87,9 +59,9 @@ if __name__ == "__main__":
         # "cycle multi list={mark list\\nextlist solid,dashed}",
         "title="+('-'.join([str(x) for x in index])),
         "legend columns=2",
-        "legend style={at={(0.1,0.9)},anchor=north west,/tikz/column 2/.style={column sep=5pt,}}",
-        xmin="0", xmax="1000",
-        ymin="-1000", ymax="2500",
+        "legend style={at={(0.1,0.1)},anchor=north west,/tikz/column 2/.style={column sep=5pt,}}",
+        xmin="0", xmax="500",
+        ymin="0", ymax="1",
         width="15cm",
         height="10cm",
         xlabel="\\textbf{Round}",
@@ -105,7 +77,7 @@ if __name__ == "__main__":
         X = xrange(start, len(expsplt[strategy][0]["utilities_mean"]) + 1, step)
         Y = expsplt[strategy][0]["utilities_mean"][start::step]
         # start += step / len(strategies)
-        texstr += coordinates(X, Y)
+        texstr += coordinates(X, Y, df = df)
     # print len(expsplt[strategy]), strategy, len(expsplt[strategy][0]["utilities_mean"])
 
     # texstr += "\\begin{scope}"
